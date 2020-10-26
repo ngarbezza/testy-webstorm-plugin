57f9b8c5d4e43d695217da2065f0706fabd05710
GitHub Action <action@github.com>
2020-10-26 04:12:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ngarbezza/testywebstormplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ngarbezza.testywebstormplugin.services
+
+import com.github.ngarbezza.testywebstormplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
